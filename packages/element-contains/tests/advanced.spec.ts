b694c9b2ff01638bfeee10a3ed4d80f924261c55
Sean Monahan <seanmonahan@microsoft.com>
2023-10-27 15:50:24 -0700

    advanced element-contains tests

--- packages/element-contains/tests/advanced.spec.ts
+++ packages/element-contains/tests/advanced.spec.ts
@@ -1,151 +1,43 @@
-// import { test, expect } from '@playwright/test';
-// import { execTest, genResult } from './execTest';
+import { test, expect } from '@playwright/test';
+import { execTest, genResult } from './execTest';
 
-// const ADVANCED_TEST_PAGE = 'http://localhost:3000/pages/advanced.html';
+const ADVANCED_TEST_PAGE = 'http://localhost:3000/pages/advanced.html';
 
 
-// test.describe('advanced custom element', () => {
-//   test('simple walk', async ({ page }) => {
-//     await page.goto(ADVANCED_TEST_PAGE);
-  
-//     const walkResult = await execTest(page, "walk");
-  
-//     expect(walkResult).toEqual([
-//       "body",
-//         "test-root",
-      
-//           "first",
-//             "first-child",
-//               "first-grand-child-one",
-//                 "basic-first-child",
-//                 "basic-second-child",
-//                 "basic-last-child",
-//               "first-grand-child-two",
+test.describe('named slot custom element', () => {
+  test.describe('light dom', () => {
+    test('contains', async ({ page }) => {
 
-//           "second",
-//             "basic-first-child",
-//             "basic-second-child",
-//             "basic-last-child",
+        await page.goto(ADVANCED_TEST_PAGE);
 
-//           "third",
-//             "default-slot-first-child",
-//             "default-slot-second-child",
-//             "slot",
-//               "third-child",
-//                 "default-slot-first-child",
-//                 "default-slot-second-child",
-//                 "slot",
-//                   "third-child-slotted",
-//                 "default-slot-last-child",
-//             "default-slot-last-child",
-      
-//           "fourth",
-//             "fourth-child",
-//               "default-slot-first-child",
-//               "default-slot-second-child",
-//               "slot",
-//                 "fourth-grand-child-one",
-//                   "named-slots-first-child",
-//                   "first-slot",
-//                     "fourth-grand-child-one-first-slot",
-//                       "basic-first-child",
-//                       "basic-second-child",
-//                       "basic-last-child",
-//                   "named-slots-second-child",
-//                   "second-slot",
-//                     "fourth-grand-child-one-second-slot",
-//                   "named-slots-last-child",
-//                   "third-slot",
-//                     "fourth-grand-child-one-third-slot",
-//               "default-slot-last-child",
-//       "test-script"
-      
-//     ]);
-//   });
-  
-// //   test.describe('shadow root', () => {
-  
-// //     test('first child', async ({ page }) => {
-// //       await page.goto(ADVANCED_TEST_PAGE);
-  
-// //       const walkResult = await execTest(page, "shadowFirstChild");
-  
-// //       expect(walkResult).toEqual([
-// //         "second",
-// //         "named-slots-first-child"
-// //       ]);
-// //     });
-  
-// //     test('last child', async ({ page }) => {
-// //       await page.goto(ADVANCED_TEST_PAGE);
-  
-// //       const walkResult = await execTest(page, "shadowLastChild");
-  
-// //       expect(walkResult).toEqual([
-// //         "second",
-// //         "third-slot"
-// //       ]);
-// //     });
-  
-// //     test('parent node', async ({ page }) => {
-// //       await page.goto(ADVANCED_TEST_PAGE);
-  
-// //       const walkResult = await execTest(page, "shadowParentNode");
-  
-// //       expect(walkResult).toEqual([
-// //         "second",
-// //         "third-slot",
-// //         "second"
-// //       ]);
-// //     });
-  
-// //     test('siblings', async ({ page }) => {
-// //       await page.goto(ADVANCED_TEST_PAGE);
-  
-// //       const walkResult = await execTest(page, "shadowSiblings");
-  
-// //       expect(walkResult).toEqual([
-// //         "second",
-// //         "named-slots-first-child",
-// //         "first-slot",
-// //         "named-slots-second-child",
-// //         "second-slot",
-// //         "named-slots-last-child",
-// //         "third-slot",
-// //         null,
-// //         "named-slots-last-child",
-// //         "second-slot",
-// //         "named-slots-second-child",
-// //         "first-slot",
-// //         "named-slots-first-child",
-// //         null
-// //       ]);
-// //     });
-  
-// //     test('next and previous node', async ({ page }) => {
-// //       await page.goto(ADVANCED_TEST_PAGE);
-  
-// //       const walkResult = await execTest(page, "shadowNextPrev");
-  
-// //       expect(walkResult).toEqual([
-// //         "second",
-// //         "named-slots-first-child",
-// //         "first-slot",
-// //         "first-slotted",
-// //         "named-slots-second-child",
-// //         "second-slot",
-// //         "second-slotted",
-// //         "named-slots-last-child",
-// //         "third-slot",
-// //         "named-slots-last-child",
-// //         "second-slotted",
-// //         "second-slot",
-// //         "named-slots-second-child",
-// //         "first-slotted",
-// //         "first-slot",
-// //         "named-slots-first-child",
-// //       ]);
-// //     });
-  
-// //   });
-// })
+        const result = await execTest(page, "lightDomContains");
+        expect(result).toEqual(genResult(21, true));
+    });
+
+    test('does not contain', async ({ page }) => {
+
+        await page.goto(ADVANCED_TEST_PAGE);
+
+        const result = await execTest(page, "lightDomNotContains");
+        expect(result).toEqual(genResult(16, false));
+
+    });
+  });
+
+  test.describe('shadow root', () => {
+
+    test('contains', async ({ page }) => {
+        await page.goto(ADVANCED_TEST_PAGE);
+
+        const result = await execTest(page, "shadowDomContains");
+        expect(result).toEqual(genResult(30, true));
+    });
+
+    test('does not contain', async ({ page }) => {
+        await page.goto(ADVANCED_TEST_PAGE);
+
+        const result = await execTest(page, "shadowDomNotContains");
+        expect(result).toEqual(genResult(27, false));
+    });
+  });
+});
